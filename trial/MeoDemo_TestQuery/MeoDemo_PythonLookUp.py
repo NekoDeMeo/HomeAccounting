e30f961b782f5b24ffdaf12317014cdda0bdec7f
Neko <nekodemeo@gmail.com>
2021-03-14 00:36:21 +0900

    HOMEACC-8: #comment Updated output DB for confirmed data #time 0h 20m

--- trial/MeoDemo_TestQuery/MeoDemo_PythonLookUp.py
+++ trial/MeoDemo_TestQuery/MeoDemo_PythonLookUp.py
@@ -1,11 +1,16 @@
 import dataset
 import pandas as pd
 
+confirmedTransaction_keyCols = ['date', 'info', 'whose', 'totalPayment', 'category']
+
 # open db
 db = dataset.connect('sqlite:///Preparation//testQuery.db')
 
 expenseLibTable = db['ExpenseLib']
 transactionTable = db['rakuten_transaction']
+
+confirmedTransactionTable = db['confirmed_transaction']
+
 
 transactionTable.create_column('info', db.types.string, nullable=True)
 transactionTable.create_column('category', db.types.string, nullable=True)
@@ -20,9 +25,22 @@ for transaction in transactions:
     if (libData != None):
         transaction['info'] = libData['what']
         transaction['category'] = libData['category']
-        print(transaction)
+        #print(transaction)
+
+        confirmedTransactionTable.insert_ignore(dict(date=transaction['date'],
+                                                     info=transaction['info'],
+                                                     whose=transaction['whose'],
+                                                     totalPayment=transaction['totalPayment'],
+                                                     category=transaction['category']
+                                                     ),
+                                                confirmedTransaction_keyCols
+                                                )
+
     else:
         print('Could not find data for transaction:', payAt)
+
+for confirmedTrans in confirmedTransactionTable.all():
+    print(confirmedTrans)
 
 ###########################
 # Open points
