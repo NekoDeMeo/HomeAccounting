f1ce05f7c4414cc73b306e5b049f18ec5b3073b1
Neko <nekodemeo@gmail.com>
2021-03-23 23:04:11 +0900

    HOMEACC-22: #comment test finish #time 0h 30m

--- trial/MeoDemo_ProgramLayout.py
+++ trial/MeoDemo_ProgramLayout.py
@@ -1,56 +1,76 @@
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import pandas as pd
+from pandas.plotting import table
 import PySimpleGUI as sg
 import matplotlib
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
 
 matplotlib.use('TkAgg')
 
-# Pie chart, where the slices will be ordered and plotted counter-clockwise:
-labels = 'Frogs', 'Hogs', 'Dogs', 'Logs'
 
-x = 15
-y = 30
-z = 45
-t = 10
+def declare_window():
+    food = 4
+    eatOut = 24
+    housing = 31
+    medical = 2
+    others = 3
 
-sizes = [x, y, z, t]
-#sizes = [15, 30, 45, 10]
-explode = (0, 0.1, 0, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
+    # sample data
+    raw_data = {'Category': ['Food', 'EatingOut', 'Housing', 'Medical', 'Others'],
+                'Payment': [food, eatOut, housing, medical, others]}
 
-fig1, ax1 = plt.subplots()
-ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
-        shadow=True, startangle=90)
-ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
-
-layout = [[sg.Text('Plot test')],
-          [sg.Canvas(key='-PieChart-')],
-          [sg.Button('Process Credit Logs')]]
-
-def make_window(theme):
-    sg.theme(theme)
+    df = pd.DataFrame(raw_data, columns = ['Category', 'Payment'])
 
     YearList = '2020', '2021'
     MonthList = 'All', '01', '02', '03', '04'
 
-    TableDataList = [['Task01', 'Value01'], ['Task02', 'Value02']]
-    headings = ["Name", "Score"]
+    main_layout = [[sg.Text('Enter File Link')],
+                   [sg.Input(key='-INTPUT'), sg.FileBrowse('Select File')],
+                   [sg.Button('Process Credit Logs'), sg.Button('Process Manual Data')],
+                   [sg.Combo(values=YearList), sg.Combo(values=MonthList)],
+                   [sg.Canvas(key='-PieChartWithTable-'), sg.Canvas(key='-GraphChart-')]
+                ]
 
-    menu_def = [['&Application', ['E&xit']],
-                ['&Help', ['&About']]]
+    window = sg.Window('Home Accounting Tool Test', main_layout, finalize=True)
 
-    right_click_menu_def = [[], ['Nothing', 'More Nothing', 'Exit']]
+    return window, df
 
-    main_layout = [[sg.Menu(menu_def, key='-MENU-')],
-                    [sg.Text('Enter File Link')],
-                    [sg.Input(key='-INTPUT'), sg.Button('Select File')],
-                    [sg.Button('Process Credit Logs'), sg.Button('Process Manual Data')],
-                    [sg.Listbox(values=YearList), sg.Listbox(values=MonthList)],
-                    [sg.Canvas(key='-PieChart-'), sg.Table(values=TableDataList, headings=headings),
-                     sg.Canvas(key='-GraphChart-')]
-                    ]
+def init_window(window, df):
+    canvas_elem = window['-PieChartWithTable-']
+    canvas = canvas_elem.TKCanvas
 
-    return sg.Window('All Elements Demo', main_layout, right_click_menu=right_click_menu_def)
+    # add the plot to the window
+    fig = Figure()
+    ax1 = fig.add_subplot(121, aspect='equal')
 
+    df.plot(kind='pie', y='Payment', ax=ax1, autopct='%1.1f%%',
+            startangle=90, shadow=False, labels=df['Category'], legend=False, fontsize=14)
+
+    ax2 = fig.add_subplot(122, aspect='equal')
+    ax2.axis('off')
+    tbl = table(ax2, df, loc='center')
+    # tbl.auto_set_font_size(True)
+
+    # plot chart
+    ax1.grid()
+    fig_agg = draw_figure(canvas, fig)
+
+    return fig_agg, ax1, ax2
+
+def update_pilechart_and_table(fig_agg, df, ax1, ax2):
+    df.at[0, 'Payment'] += 1
+    ax1.cla()
+    ax1.axis('off')
+    df.plot(kind='pie', y='Payment', ax=ax1, autopct='%1.1f%%',
+            startangle=90, shadow=False, labels=df['Category'], legend=False, fontsize=14)
+
+    ax2.cla()
+    ax2.axis('off')
+    tbl = table(ax2, df, loc='center')
+
+    fig_agg.draw()
 
 def draw_figure(canvas, figure):
     figure_canvas_agg = FigureCanvasTkAgg(figure, canvas)
@@ -59,14 +79,13 @@ def draw_figure(canvas, figure):
     return figure_canvas_agg
 
 def main():
-    global y, z
-    #window = make_window(sg.theme())
 
-    window = sg.Window('Demo Application - Embedding Matplotlib In PySimpleGUI', layout, finalize=True,
-                       element_justification='center', font='Helvetica 18')
+    # create the form and show it without the plot
+    window, df = declare_window()
 
-    # add the plot to the window
-    fig_canvas_agg = draw_figure(window['-PieChart-'].TKCanvas, fig1)
+    fig_agg, ax1, ax2 = init_window(window, df)
+
+
 
     # This is an Event Loop
     while True:
@@ -83,22 +102,9 @@ def main():
             break
         elif event == 'Process Credit Logs':
             print("[LOG] Clicked Process Credit Logs Button!")
-
-            y = y + 1
-            z = z - 1
-
-            fig_canvas_agg = draw_figure(window['-PieChart-'].TKCanvas, fig1)
-            #TODO: how to update canvas live?
-
-            sg.popup("You pressed a Process Credit Logs button!")
-            print("[LOG] Dismissing Popup!")
+            update_pilechart_and_table(fig_agg, df, ax1, ax2)
         elif event == 'Process Manual Data':
             print("[LOG] Clicked Process Manual Data Button!")
-            y = y - 1
-            z = z + 1
-
-            sg.popup("You pressed a Process Manual Data button!")
-            print("[LOG] Dismissing Popup!")
 
     window.close()
     exit(0)
